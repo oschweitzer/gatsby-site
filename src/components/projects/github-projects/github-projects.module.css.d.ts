df9dd52dcecd2faf24e19de7977432130dffeae9
Oli1810 <olivier.sch68@gmail.com>
2019-07-31 15:42:18 +0200

    feat: add links to Github projects

---
+++ src/components/projects/github-projects/github-projects.module.css.d.ts
@@ -0,0 +1,3 @@
+export const projectCard: string;
+export const hvrPush: string;
+export const projectChip: string;
--- src/components/projects/github-projects/github-projects.tsx
+++ src/components/projects/github-projects/github-projects.tsx
@@ -1,7 +1,8 @@
-import { Card, CardContent, CardHeader, Grid } from "@material-ui/core";
+import { Card, CardContent, CardHeader, Chip, Grid } from "@material-ui/core";
 import { graphql, StaticQuery } from "gatsby";
 import React from "react";
 import * as navigationStyles from "../../navigation-cards/navigation-cards.module.css";
+import * as styles from "./github-projects.module.css";
 
 export default () => (
   <StaticQuery
@@ -14,10 +15,19 @@ export default () => (
               privacy: PUBLIC
               isFork: false
               ownerAffiliations: OWNER
+              orderBy: { field: NAME, direction: ASC }
             ) {
               nodes {
                 name
                 description
+                url
+                repositoryTopics(first: 20) {
+                  nodes {
+                    topic {
+                      name
+                    }
+                  }
+                }
               }
             }
           }
@@ -27,17 +37,29 @@ export default () => (
     render={data => (
       <Grid container justify="center" spacing={3}>
         {data.github.viewer.repositories.nodes.map(repo => (
-          <Grid item key={repo.name} xs={4}>
-            <Card>
-              <CardHeader
-                className={navigationStyles.cardHeader}
-                title={repo.name}
-                titleTypographyProps={{ variant: "h5" }}
-              ></CardHeader>
-              <CardContent className={navigationStyles.cardContent}>
-                {repo.description}
-              </CardContent>
-            </Card>
+          <Grid item key={repo.name} xs={3}>
+            <a href={repo.url} style={{ textDecoration: "none" }}>
+              <Card className={styles.projectCard}>
+                <CardHeader
+                  className={navigationStyles.cardHeader}
+                  title={repo.name}
+                  titleTypographyProps={{ variant: "h5" }}
+                ></CardHeader>
+                <CardContent className={navigationStyles.cardContent}>
+                  {repo.description}
+                  <Grid item xs={12}>
+                    {repo.repositoryTopics.nodes.map(topic => (
+                      <Chip
+                        key={topic.topic.name}
+                        label={topic.topic.name}
+                        className={styles.projectChip}
+                        color="primary"
+                      ></Chip>
+                    ))}
+                  </Grid>
+                </CardContent>
+              </Card>
+            </a>
           </Grid>
         ))}
       </Grid>
