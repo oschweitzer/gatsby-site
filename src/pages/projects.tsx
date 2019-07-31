3bd0c54aac9ced89efae0032cc99210429214ebf
Oli1810 <olivier.sch68@gmail.com>
2019-07-31 17:25:18 +0200

    refactor: use Container to structure the projects page

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,16 +1,20 @@
+import Container from "@material-ui/core/Container";
 import React from "react";
 import GithubProjects from "../components/projects/github-projects/github-projects";
 import OtherProjects from "../components/projects/other-projects/other-projects";
-import { Divider } from "@material-ui/core";
-import * as styles from "./projects.module.css";
 
 export default () => (
-  <div>
-    <h1>Projects</h1>
-    <h2>Personal projects</h2>
-    <GithubProjects></GithubProjects>
-    <Divider className={styles.divider}></Divider>
-    <h2>Work experience</h2>
-    <OtherProjects></OtherProjects>
-  </div>
+  <React.Fragment>
+    <Container maxWidth="lg">
+      <div>
+        <h1>Projects</h1>
+        <h2>Personal projects</h2>
+        <GithubProjects></GithubProjects>
+      </div>
+    </Container>
+    <Container>
+      <h2>Work experience</h2>
+      <OtherProjects></OtherProjects>
+    </Container>
+  </React.Fragment>
 );
