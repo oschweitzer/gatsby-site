6a347e887e203ffd1d8660da5cbd2dff7d6f18b0
Oli1810 <olivier.sch68@gmail.com>
2019-09-07 17:25:27 +0200

    refactor(titles): align section icon with text

--- src/components/titles/section.tsx
+++ src/components/titles/section.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const SectionTitle = ({children, text}) => {
   return (
-    <h2 className='title is-2' style={{color: 'var(--textNormal)'}}>
-      {children}
+    <h2 className='title is-2 level-left' style={{color: 'var(--textNormal)'}}>
+      <span className={'level-left'} style={{marginRight: '10px'}}>{children}</span>
       {text}</h2>
   )
 };
