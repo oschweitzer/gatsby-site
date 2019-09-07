c39a5cc8df1cc7d754ae9033b4096abaaf1954a0
Oli1810 <olivier.sch68@gmail.com>
2019-09-07 21:12:16 +0200

    refactor: specify config when using dark mode plugin

--- src/components/theme-toggle/theme-toggle.tsx
+++ src/components/theme-toggle/theme-toggle.tsx
@@ -4,13 +4,18 @@ import useDarkMode from 'use-dark-mode';
 import styles from './theme-toggle.module.scss';
 
 const ThemeToggle = () => {
-  const darkMode = useDarkMode(false);
+  const darkMode = useDarkMode(false, {
+    classNameDark: 'dark-mode',
+    classNameLight: 'light-mode',
+    storageKey: "darkMode"}
+  );
   const icon = darkMode.value ? <FaMoon color={'#F5F3CE'}/> : <FaSun color={'#F28C38'}/>;
 
   return (
     <div className={styles.themeToggle}>
       <div className='field' title={"Activate/deactivate Dark mode"}>
-        <input aria-label='Dark mode switch' id='switchRoundedDefault' type='checkbox' name='switchRoundedDefault' className='switch is-rounded is-info'
+        <input aria-label='Dark mode switch' id='switchRoundedDefault' type='checkbox'
+               name='switchRoundedDefault' className='switch is-rounded is-info'
                checked={darkMode.value} onChange={darkMode.toggle} />
         <label htmlFor='switchRoundedDefault'>{icon}</label>
       </div>
